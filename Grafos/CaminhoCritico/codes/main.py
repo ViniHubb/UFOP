78bedbb7419a79886368456c225123741f7e3dd4
Vinicius <92757894+ViniHubb@users.noreply.github.com>
2023-03-22 22:33:58 -0300

    Tirando 'print(grafo)'

--- Grafos/CaminhoCritico/codes/main.py
+++ Grafos/CaminhoCritico/codes/main.py
@@ -6,7 +6,6 @@ while True:
     if arquivo == "0": break
     lista = grafo.leitor_arquivo(arquivo)
     grafo = grafo.cria_adj(lista)
-    print(grafo)
     dist = grafo.dijkstra_pq_reverse(0)[0]
     pred = grafo.dijkstra_pq_reverse(0)[1]
     grafo.reposta(pred, lista)
